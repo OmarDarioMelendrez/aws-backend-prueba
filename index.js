f07803f665e4200d2dbbc68da32fcb97b43feac8
Hitoki <melendrezomar2@gmail.com>
2021-07-26 22:46:59 -0300

    add morgan middleware

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const morgan = require('morgan')
+
+app.use(morgan('tiny'))
 
 app.get('/', (req, res) => {
   res.send('Hello Aws app!')
